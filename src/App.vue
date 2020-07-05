9a6c94bd2353386683edef3fef9cfa5808887e7b
aso1901103 <1901103@s.asojuku.ac.jp>
2020-07-05 07:30:07 +0000

    list_app

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Mylist v-bind:msg="title" v-on:result-event="appAct" />
+    <hr>
+    <table v-html="list" align='center' border='1'></table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Mylist from './components/Mylist.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Mylist
+  },
+  data:function(){
+    return {
+      title:'メモを入力してください',
+      memo:[],
+    }
+  },
+  computed:{
+    list:function(){
+      var table = '<tr><th class="head">my list</tr></tr>';
+      for(var i in this.memo){
+        table += '<tr><td>' + this.memo[i] + '</td></tr>';
+      }
+      return table;
+    }
+  },
+  created:function(){
+    var items = localStorage.getItem('list');
+    var lists = JSON.parse(items);
+    if(lists != null){ this.result = lists; }
+  },
+  methods:{
+    appAct:function(input){
+      this.memo.unshift(input);
+      if(this.memo.length > 5){
+        this.memo.pop();
+      }
+      var list = JSON.stringify(this.result);
+      localStorage.setItem('list',list);
+    },
+  },
+  
 }
 </script>
 
---
+++ src/components/Mylist.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>{{msg}}</h1>
+        <input type="text" v-model="input">
+        <button v-on:click='doAct'>クリック</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Mylist',
+  props: {
+    msg: String,
+  },
+  data:function(){
+    return {
+      input: '',
+    };
+  },
+  methods:{
+      doAct(){
+          this.$emit('result-event',this.input);
+      }
+  }
+}
+</script>
